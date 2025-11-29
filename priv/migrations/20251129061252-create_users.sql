1bde150425eac92e62eb6904a0b19a068b62a830
Ryan McBride <mr.ryan444@gmail.com>
2025-11-28 23:16:31 -0700

    Add create_users migration

---
+++ priv/migrations/20251129061252-create_users.sql
@@ -0,0 +1,10 @@
+--- migration:up
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+--- migration:down
+DROP TABLE IF EXISTS users;
+--- migration:end
